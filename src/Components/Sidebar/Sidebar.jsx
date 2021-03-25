405ebde241f4785041e2d8e3c7a63925924734e3
Kushank Sriraj <kushanksriraj@gmail.com>
2021-03-25 10:13:19 +0530

    style: sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,5 +2,13 @@ import "../../chassis.css";
 import "./sidebar.css";
 
 export const Sidebar = () => {
-  return <aside>This is the sidebar.</aside>;
+  return (
+    <aside>
+      <ul className="list__stacked">
+        <li>Getting started</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </aside>
+  );
 };
